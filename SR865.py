ae47d416566977dceedce7110190ba4c4f68ac80
Warm Vapor Control <dihm.meyer@gmail.com>
2017-08-30 14:55:14 -0400

    Added future imports to SR865. Also updated how it tracks sens and tau integers to allow for coercion. This allows for full implemenation of phase, sens, tau for complete remote control.

--- SR865.py
+++ SR865.py
@@ -5,19 +5,24 @@
 #                                                                   #
 #####################################################################
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import numpy as np
 from labscript_devices import labscript_device, BLACS_tab, BLACS_worker
 from labscript_devices.VISA import VISA, VISATab, VISAWorker
 from labscript import Device, AnalogOut, config, LabscriptError, set_passed_properties
 import labscript_utils.properties
 
-sens = {0:1, 1:500e-3, 2:200e-3, 3:100e-3, 4:50e-3, 5:20e-3, 6:10e-3, 7:5e-3, 8:2e-3, 9:1e-3, 
-         10:500e-6, 11:200e-6, 12:100e-6, 13:50e-6, 14:20e-6, 15:10e-6, 16:5e-6, 17:2e-6, 18:1e-6, 19:500e-9,
-        20:200e-9, 21:100e-9, 22:50e-9, 23:20e-9, 24:10e-9, 25:5e-9, 26:2e-9, 27:1e-9}
-revsens = dict((val,key) for key,val in sens.items())
-tau = {0:1e-6, 1:3e-6, 2:10e-6, 3:30e-6, 4:100e-6, 5:300e-6, 6: 1e-3, 7:3e-3, 8:10e-3, 9:30e-3,
-      10:100e-3, 11:300e-3, 12:1, 13:3, 14:10, 15:30, 16:100, 17:300, 18: 1e3, 19:3e3, 20:10e3, 21:30e3}
-revtau = dict((val,key) for key,val in tau.items())
+sens = np.array([1,500e-3,200e-3,100e-3,50e-3,20e-3,10e-3,5e-3,2e-3,1e-3,
+                    500e-6,200e-6,100e-6,50e-6,20e-6,10e-6,5e-6,2e-6,1e-6,
+                    500e-9,200e-9,100e-9,50e-9,20e-9,10e-9,5e-9,2e-9,1e-9])
+                    
+tau = np.array([1e-6,3e-6,10e-6,30e-6,100e-6,300e-6,
+                1e-3,3e-3,10e-3,30e-3,100e-3,300e-3,
+                1,3,10,30,100,300,1e3,3e3,10e3,30e3])
         
 @labscript_device              
 class SR865(VISA):
@@ -27,6 +32,7 @@ class SR865(VISA):
     # initialize these parameters to None
     tau = None
     sens = None
+    phase = None
 
     @set_passed_properties()
     def __init__(self, name, VISA_name):
@@ -36,28 +42,34 @@ class SR865(VISA):
         
     def set_tau(self, tau_constant):
         '''Set the time constant in seconds.
-        Uses dictionary to translate to int values.'''
+        Uses numpy digitize to translate to int values.'''
         self.tau = tau_constant
         # check that setting is valid
-        try:
-            self.tau_i = revtau[tau_constant]
-        except KeyError as e:
+        if tau_constant in tau:
+            self.tau_i = np.digitize(tau_constant,tau,right=True)
+        else:
             raise LabscriptError('{:s}: tau cannot be set to {:f}'.format(self.VISA_name,self.tau))
         
     def set_sens(self, sensitivity):
         '''Set the sensitivity in Volts
-        Uses dictionary to translated to int values.'''
+        Uses numpy digitize to translate to int values.'''
         self.sens = sensitivity
         # check that setting is valid
-        try:
-            self.sens_i = revsens[sensitivity]
-        except KeyError as e:
+        if sensitivity in sens:
+            self.sens_i = np.digitize(sensitivity,sens)
+        else:
             raise LabscriptError('{:s}: sensitivity cannot be set to {:f}'.format(self.VISA_name,self.sens))
+            
+    def set_phase(self, phase):
+        '''Set the phase reference in degrees
+        Device auto-converts to -180,180 range'''
+        self.phase = phase
+        
     
     def generate_code(self, hdf5_file):
         # type the static_table
-        static_dtypes = [('tau', np.float16), ('tau_i', np.int8)] + \
-                        [('sens', np.float16), ('sens_i', np.int8)]
+        static_dtypes = np.dtype({'names':['tau','tau_i','sens','sens_i','phase'],
+                            'formats':[np.float16,np.int8,np.float16,np.int8,np.float32]})
         static_table = np.zeros(1,dtype=static_dtypes)
         
         # if tau is set, add tau value to table, else add NaN
@@ -74,25 +86,22 @@ class SR865(VISA):
         else:
             static_table['sens'] = np.NaN
             static_table['sens_i'] = -1
+        # if phase set, add to table, else NaN
+        if self.phase:
+            static_table['phase'] = self.phase
+        else:
+            static_table['phase'] = np.NaN
 
         grp = hdf5_file.create_group('/devices/'+self.name)
         grp.create_dataset('STATIC_DATA',compression=config.compression,data=static_table) 
         # add these values to device properties for easy lookup
-        self.set_property('tau', self.tau, location='device_properties')
-        self.set_property('sensitivity', self.sens, location='device_properties')
+        if self.tau: self.set_property('tau', self.tau, location='device_properties')
+        if self.sens: self.set_property('sensitivity', self.sens, location='device_properties')
+        if self.phase: self.set_property('phase',self.phase,location='device_properties')
 
 
 from blacs.tab_base_classes import define_state
-from blacs.tab_base_classes import MODE_MANUAL, MODE_TRANSITION_TO_BUFFERED, MODE_TRANSITION_TO_MANUAL, MODE_BUFFERED
-import sys  
-
-# Imports for handling icons in STBstatus.ui
-if 'PySide' in sys.modules.copy():
-    from PySide import QtCore
-    from PySide import QtGui
-else:
-    from PyQt4 import QtCore
-    from PyQt4 import QtGui
+from blacs.tab_base_classes import MODE_MANUAL, MODE_TRANSITION_TO_BUFFERED, MODE_TRANSITION_TO_MANUAL, MODE_BUFFERED 
 
 @BLACS_tab
 class SR865Tab(VISATab):
@@ -107,18 +116,6 @@ class SR865Tab(VISATab):
                           'bit 1':'unused',
                           'bit 0':'unused'}
     
-    #status_widget = 'SR865.ui'
-    
-    # setup for the sens and tau widgets
-    #self._tau_comboboxmodel = QtGui.QStandardItemModel()
-    #self._sens_comboboxmodel = QtGui.QStandardItemModel()
-    
-    #for val in tau:
-    #    self._tau_comobboxmodel.appendRow(QStandardItem(val))
-        
-    #for val in sens:
-    #    self._sens_comobboxmodel.appendRow(QStandardItem(val))
-    
     def __init__(self,*args,**kwargs):
         # set the worker
         self.device_worker_class = SR865Worker
@@ -126,8 +123,7 @@ class SR865Tab(VISATab):
     
     def initialise_GUI(self):
         
-        # use temporary AO widgets to report current settings and allow smart programming
-        # these widgets will not allow for manual programming
+        # use AO widgets to mimick functionality
         ao_prop = {'tau':{'base_unit':'s',
                           'min':1e-6,
                           'max':30e3,
@@ -139,8 +135,8 @@ class SR865Tab(VISATab):
                             'step':1e-3,
                             'decimals':9},
                     'phase':{'base_unit':'deg',
-                             'min':-360,
-                             'max':360,
+                             'min':-180,
+                             'max':180,
                              'step':1,
                              'decimals':6}}
                             
@@ -150,14 +146,6 @@ class SR865Tab(VISATab):
         
         # call VISATab.initialise to create SR865 widget
         VISATab.initialise_GUI(self)
-        
-        # populate combo boxes and set signals for them
-        #self.status_ui.tau_comboBox.setModel(self._tau_comboboxmodel)
-        #self.status_ui.sens_comboBox.setModel(self._sens_comboboxmodel)
-        
-        #self.status_ui.tau_comboBox.currentIndexChanged(self.tau_changed)
-        #self.status_ui.sens_comboBox.currentIndexChanged(self.sens_changed)
-        #self.status_ui.phase_SpinBox.
 
         # Set the capabilities of this device
         self.supports_remote_value_check(True)
@@ -189,20 +177,38 @@ class SR865Tab(VISATab):
             self.status_ui.tau_comboBox.blockSignals(False)
 
 @BLACS_worker
-class SR865Worker(VISAWorker):    
+class SR865Worker(VISAWorker):
+    program_string = 'OFLT {:d};SCAL {:d};PHAS {:.6f}'
+    read_string = 'OFLT?;SCAL?;PHAS?'   
     
     def phase_parser(self,phase_string):
         '''Phase Query string parser'''
         phase = float(phase_string)
         return phase
+        
+    def coerce_tau(self,tau_constant):
+        '''Returns coerced, valid integer setting. 
+        Tau value rounds up.
+        Returns max or min valid setting if out of bound.'''
+        coerced_i = int(np.digitize(tau_constant,tau,right=True))
+        if coerced_i >= len(tau):
+            coerced_i -= 1
+        return coerced_i
+        
+    def coerce_sens(self,sensitivity):
+        '''Returns coerced, valid integer setting. 
+        Sens value rounds down.
+        Returns max or min valid setting if out of bound.'''
+        coerced_i = int(np.digitize(sensitivity,sens))
+        if coerced_i >= len(sens):
+            coerced_i -= 1
+        return coerced_i
     
     def init(self):
         # import h5py with locks
         global h5py; import labscript_utils.h5_lock, h5py
         # Call the VISA init to initialise the VISA connection
         VISAWorker.init(self)
-        # longer timeout for potential future data reads
-        #self.connection.timeout = 10000
         
         # initial configure of the instrument
         self.connection.read_termination = u'\n'
@@ -213,13 +219,9 @@ class SR865Worker(VISAWorker):
     
     def check_remote_values(self):
         # Get the current set values:
-
         results = {}
-        
-        # query instrument
-        [tau_i, sens_i, phase] = self.connection.query_ascii_values('OFLT?;SCAL?;PHAS?',separator=';')
-        #results['tau_i'] = int(tau_i)
-        #results['sens_i'] = int(sens_i)
+
+        [tau_i, sens_i, phase] = self.connection.query_ascii_values(self.read_string,separator=';')
             
         # convert to proper numbers
         results['tau'] = tau[int(tau_i)]
@@ -228,26 +230,23 @@ class SR865Worker(VISAWorker):
         
         if (self.smart_cache['STATIC_DATA'] is not None) and (results != self.smart_cache['STATIC_DATA']):
             # remote values changed, need to reprogram on next run
-            print('================RESET=======================')
             self.smart_cache['STATIC_DATA'] = None
 
         return results
     
     def program_manual(self,front_panel_values):
-        #tau_constant = front_panel_values['tau']
-        #sensitivity = front_panel_values['sens']
-        #phase = front_panel_values['phase']
+        # coerce the front_panel tau & sens to a valid setting
+        tau_i = self.coerce_tau(front_panel_values['tau'])
+        sens_i = self.coerce_sens(front_panel_values['sens'])
+        phase = front_panel_values['phase']
         
-        #self.connection.write('OFLT {:d};SCAL {:d};PHAS {:.6f}'.format(revtau[tau_constant],
-        #                                                    revsens[sensitivity],
-        #                                                    phase))
-        
+        self.connection.write(self.program_string.format(tau_i,sens_i,phase))
+                
         return self.check_remote_values()        
 
     def transition_to_buffered(self,device_name,h5file,initial_values,fresh):
         # call parent method to do basic preamble
         VISAWorker.transition_to_buffered(self,device_name,h5file,initial_values,fresh)
-        # Program static values
         
         data = None
         with h5py.File(h5file) as hdf5_file:
@@ -256,42 +255,35 @@ class SR865Worker(VISAWorker):
             if 'STATIC_DATA' in group:
                 data = group['STATIC_DATA'][:][0]
                 
-        # don't forget to check if it needs to be programmed
-        # by smart programming
-        # and if setting isn't called for a change either
+        # Save these values into final_values so the GUI can
+        # be updated at the end of the run to reflect them:
+        # assume initial values in case something isn't programmed
+        self.final_values = self.initial_values
         
         if data is not None:
-            if fresh or data != self.smart_cache['STATIC_DATA']:
-                # Save these values into final_values so the GUI can
-                # be updated at the end of the run to reflect them:
-                # assume initial values in case something isn't programmed
-                final_values = self.initial_values
-                
+            if fresh or data != self.smart_cache['STATIC_DATA']:                
                 if data['tau_i'] != -1:
                     self.connection.write('OFLT {:d}'.format(data['tau_i']))
-                    final_values['tau'] = tau[data['tau_i']]
+                    self.final_values['tau'] = tau[data['tau_i']]
+                else:
+                    self.final_values['tau'] = tau[initial_values['tau_i']]
                 if data['sens_i'] != -1:
                     self.connection.write('SCAL {:d}'.format(data['sens_i']))
-                    final_values['sens'] = sens[data['sens_i']]
+                    self.final_values['sens'] = sens[data['sens_i']]
+                else:
+                    self.final_values['sens'] = sens[initial_values['sens_i']]
+                if not np.isnan(data['phase']):
+                    self.connection.write('PHAS {:.6f}'.format(data['phase']))
+                    self.final_values['phase'] = data['phase']
+                else:
+                    self.final_values['phase'] = initial_values['phase']
+                    
+        # write the final_values to h5file for later lookup
+        with h5py.File(h5file) as hdf5_file:
+            group = hdf5_file['/devices/'+device_name]
+            group.attrs.create('sensitivity',self.final_values['sens'])
+            group.attrs.create('tau',self.final_values['tau'])
+            group.attrs.create('phase',round(self.final_values['phase'],6))
                 
-        return final_values
-        
-    def transition_to_manual(self,abort = False):
-        '''Save relevant parameters to h5file.'''         
-        if abort:
-            # If we're aborting the run, reset to original value
-            self.program_manual(self.initial_values)
-        # If we're not aborting the run, stick with buffered value. Nothing to do really!
-        if not abort:
-            # phase data to h5file as device attribute
-            # first read the phase from the device, without h5lock
-            
-            phase = self.phase_parser(self.connection.query('PHAS?'))
-            
-            # add to h5file
-            with h5py.File(self.h5_file,'a') as hdf5_file:
-                dev_grp = hdf5_file['/devices/'+self.device_name]
-                dev_grp.attrs.create('phase',phase)
-                
-        return True
+        return self.final_values
 
