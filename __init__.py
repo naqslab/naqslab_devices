0d31d1973443d259a15307bc127b421771363b2a
David Meyer <dihm.meyer@gmail.com>
2022-03-04 13:22:52 -0500

    Clean up StaticFreqAmp class to properly inherit StatisDDS.
    Allows for proper gates of SigGens to be specified. Still blocks
    setting phase parameters.

--- __init__.py
+++ __init__.py
@@ -20,15 +20,17 @@ __author__ = ['dihm']
 ##############################################
 # define helper sub-classes of labscript defined channels
 
-from labscript import Device, AnalogIn, StaticDDS, StaticAnalogQuantity, LabscriptError, set_passed_properties
+from labscript import Device, AnalogIn, StaticDDS, LabscriptError
+
 
 class ScopeChannel(AnalogIn):
     """Subclass of labscript.AnalogIn that marks an acquiring scope channel.
     """
     description = 'Scope Acquisition Channel Class'
+
     def __init__(self, name, parent_device, connection):
         """This instantiates a scope channel to acquire during a buffered shot.
-        
+
         Args:
             name (str): Name to assign channel
             parent_device (obj): Handle to parent device
@@ -38,23 +40,25 @@ class ScopeChannel(AnalogIn):
         """
         Device.__init__(self,name,parent_device,connection)
         self.acquisitions = []
-        
+
     def acquire(self):
         """Inform BLACS to save data from this channel.
-        
+
         Note that the parent_device controls when the acquisition trigger is sent.
         """
         if self.acquisitions:
             raise LabscriptError('Scope Channel {0:s}:{1:s} can only have one acquisition!'.format(self.parent_device.name,self.name))
         else:
             self.acquisitions.append({'label': self.name})
-           
+
+
 class CounterScopeChannel(ScopeChannel):
     """Subclass of :obj:`ScopeChannel` that allows for pulse counting."""
     description = 'Scope Acquisition Channel Class with Pulse Counting'
+
     def __init__(self, name, parent_device, connection):
         """This instantiates a counter scope channel to acquire during a buffered shot.
-        
+
         Args:
             name (str): Name to assign channel
             parent_device (obj): Handle to parent device
@@ -63,13 +67,13 @@ class CounterScopeChannel(ScopeChannel):
                               the channel label.
         """
         ScopeChannel.__init__(self,name,parent_device,connection)
-        self.counts = []                       
-        
+        self.counts = []
+
     def count(self,typ,pol):
         """Register a pulse counter operation for this channel.
-        
+
         Args:
-            typ (str): count 'pulse' or 'edge' 
+            typ (str): count 'pulse' or 'edge'
             pol (str): reference to 'pos' or 'neg' edges
         """
         # guess we can allow multiple types of counters per channel
@@ -78,37 +82,37 @@ class CounterScopeChannel(ScopeChannel):
         else:
             raise LabscriptError('Invalid counting parameters for {0:s}:{1:s}'.format(self.parent_name,self.name)) 
 
+
 class StaticFreqAmp(StaticDDS):
     """A Static Frequency that supports frequency and amplitude control.
-    
+
     If phase control is needed, use labscript.StaticDDS"""
     description = 'Frequency Source class for Signal Generators'
-    allowed_children = [StaticAnalogQuantity]
-       
-    def __init__(self, name, parent_device, connection, freq_limits = None, freq_conv_class = None,freq_conv_params = {}, amp_limits = None, amp_conv_class = None, amp_conv_params = {}):
+
+    def __init__(self, *args, **kwargs):
         """This instatiates a static frequency output channel.
-        
-        Frequency and amplitude limits set here will supercede those dictated 
-        by the device class, but only when compiling a shot with runmanager. 
+
+        Frequency and amplitude limits set here will supercede those dictated
+        by the device class, but only when compiling a shot with runmanager.
         Static update limits are enforced by the BLACS Tab for the parent device.
-        
+
         Args:
-            name (str): Name to assign output channel
-            parent_device (obj): Handle to parent device
-            connection (str): Which physical channel to use on parent device.
-                              Typically only 'Channel 0' is available.
-            freq_limits (tuple): Set (min,max) output frequencies in BLACS front panel units.
-            freq_conv_class (obj): Custom conversion class to use
-            freq_conv_params (dict): Parameters to conversion class
-            amp_limits (tuple): Set (min,max) output amplitude in BLACS front panel units.
-            amp_conv_class (obj): Custom convsersion class to use
-            amp_conv_params (dict): Parameters to conversion class
+            *args: Passed to parent init.
+            **kwargs: Passed to parent init.
+
+        Raises:
+            LabscriptError: If **kwargs contains phase settings, which are not supported.
         """
-        Device.__init__(self,name,parent_device,connection)
-        self.frequency = StaticAnalogQuantity(self.name+'_freq',self,'freq',freq_limits,freq_conv_class,freq_conv_params)
-        self.amplitude = StaticAnalogQuantity(self.name+'_amp',self,'amp',amp_limits,amp_conv_class,amp_conv_params)
+
+        if not {'phase_limits','phase_conv_class','phase_conv_params'}.isdisjoint(kwargs.keys()):
+            raise LabscriptError(f'{self.device.name} does not support any phase configurations.')
+
+        super().__init__(*args,**kwargs)
         # set default values within limits specified
         # if not specified, use limits from parent device
+        parent_device = args[1]
+        freq_limits = kwargs.get('freq_limits')
+        amp_limits = kwargs.get('amp_limits')
         if freq_limits is not None:
             self.frequency.default_value = freq_limits[0]
         else:
@@ -117,20 +121,9 @@ class StaticFreqAmp(StaticDDS):
             self.amplitude.default_value = amp_limits[0]
         else:
             self.amplitude.default_value = parent_device.amp_limits[0]/parent_device.amp_scale_factor
-        
+
     def setphase(self,value,units=None):
         """Overridden from StaticDDS so as not to provide phase control, which
         is generally not supported by :obj:`SignalGenerator` devices.
         """
         raise LabscriptError('StaticFreqAmp does not support phase control')
-            
-    def enable(self):       
-        """overridden from StaticDDS so as not to provide time resolution -
-        output can be enabled or disabled only at the start of the shot"""
-        raise LabscriptError('StaticFreqAmp {:s} does not support a digital gate'.format(self.name))
-                            
-    def disable(self):
-        """overridden from StaticDDS so as not to provide time resolution -
-        output can be enabled or disabled only at the start of the shot"""
-        raise LabscriptError('StaticFreqAmp {:s} does not support a digital gate'.format(self.name))
-
